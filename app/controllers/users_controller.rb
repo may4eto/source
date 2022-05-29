000056fc7f91a25a3647f820685be6b7d71de1cf
Maya Mircheva <may4eto@hotmail.com>
2022-05-29 23:04:20 +0200

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    def create
+        @user = User.new(form_params)
+        if  @user.save
+            redirect_to root_path
+        else
+            render "new"
+        end
+    end
+    def form_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+end
